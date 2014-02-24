9c200c0f39c8d418ae2ab2893466de12f8e341a9
Gerry Said <gerrysaid@gmail.com>
2014-02-24 12:45:43 +0100

    generate 4 asteroids
    generate 4 asteroids

---
+++ Assets/ComponentLibrary/AsteroidGenerator.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+var asteroid:Rigidbody;
+
+
+function Start () {
+	//asteroid 1
+	var xpos = Random.Range(-10,10);
+	var ypos = Random.Range(-10,10);
+	Instantiate(asteroid,Vector3(xpos,ypos,0),Quaternion.identity);
+	
+	//asteroid 2
+	xpos = Random.Range(-10,10);
+	ypos = Random.Range(-10,10);
+	Instantiate(asteroid,Vector3(xpos,ypos,0),Quaternion.identity);
+	
+	//asteroid 3
+	xpos = Random.Range(-10,10);
+	ypos = Random.Range(-10,10);
+	Instantiate(asteroid,Vector3(xpos,ypos,0),Quaternion.identity);
+	
+	//asteroid 4
+	xpos = Random.Range(-10,10);
+	ypos = Random.Range(-10,10);
+	Instantiate(asteroid,Vector3(xpos,ypos,0),Quaternion.identity);
+
+}
+
+function Update () {
+
+}
